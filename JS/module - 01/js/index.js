f7ec6c4cebe8fc8be2eb791e127007383d6babcb
Valya94 <tetyk.v@gmail.com>
2018-06-23 11:54:32 +0300

    add mod-01 js

--- JS/module - 01/js/index.js
+++ JS/module - 01/js/index.js
@@ -1,72 +1,26 @@
-"use strict";
+'use strict';
 
-const tourTaba = 6;
-const tourSharm = 15;
-const tourHurgada = 25;
+const ADMIN_LOGIN = 'admin';
+const ADMIN_PASSWORD = 'm4ngo1zh4ackz0r';
+const cancel = 'Отменено пользователем!';
+const ban = 'Доступ запрещен!';
+const welcome = 'Добро пожаловать!';
 
 
-const tourUser = prompt('Введите необходимое количество мест: ');
-console.log(tourUser);
-console.log(typeof tourUser);
+const log = prompt('Введите ваш логин');
 
-if (typeof Number.parseInt(tourUser) === 'number' && !Number.isNaN(tourUser) && (Number.parseInt(tourUser) > 0)) {
-    if (tourUser <= tourTaba) {
-       let answerUser = confirm ('Есть места в группе Taba. Вы согласны быть в этой группе?');
-       if (answerUser) {
-            tourTaba - tourUser;
-            alert('Приятного путешествия в группе Taba!');
-       } else {
-           if (tourUser <= tourSharm) {
-                let answerUser = confirm ('Есть места в группе Sharm. Вы согласны быть в этой группе?');
-                if (answerUser) {
-                    tourSharm - tourUser;
-                    alert('Приятного путешествия в группе Sharm!');
-                } else {
-                    if (tourUser <= tourHurgada) {
-                        let tourUser = confirm ('Есть места в группе Hurgada. Вы согласны быть в этой группе?');
-                        if (tourUser) {
-                            tourHurgada - tourUser;
-                            alert('Приятного путешествия в группе Hurgada!');
-                        } else {
-                            alert('Изините, мест нет.');
-                        }
-                   }
-               }
-           }
-       }
-    } else {
-        if (tourUser <= tourSharm) {
-            let answerUser = confirm ('Есть места в группе Sharm. Вы согласны быть в этой группе?');
-            if (answerUser) {
-                tourSharm - tourUser;
-                alert('Приятного путешествия в группе Sharm!');
-        } else {
-            if (tourUser <= tourHurgada) {
-                let answerUser = confirm ('Есть места в группе Hurgada. Вы согласны быть в этой группе?');
-                if (answerUser) {
-                    tourHurgada - tourUser;
-                    alert('Приятного путешествия в группе Hurgada!');
-             } else {
-                 alert('Изините, мест нет.');
-                }
-            }
-        }
-    
-   } else {
-       if (tourUser <= tourHurgada) {
-        let answerUser = confirm ('Есть места в группе Hurgada. Вы согласны быть в этой группе?');
-        if (answerUser) {
-            tourHurgada - tourUser;
-            alert('Приятного путешествия в группе Hurgada!');
-        } else {
-            alert('Изините, мест нет.');
-            }
-        }
-        else {
-            alert('Изините, мест нет.');
-        }
-    }
-}
+if (log === null) {
+    alert(cancel);
+} else if (log !== ADMIN_LOGIN) {
+    alert(ban);
 } else {
-    alert('Ошибка ввода.');
+    let pass = prompt('Введите пароль');
+
+    if (pass === null) {
+        alert(cancel);
+    } else if (pass !== ADMIN_PASSWORD) {
+        alert(ban);
+    } else {
+        alert(welcome);
+    }
 }
